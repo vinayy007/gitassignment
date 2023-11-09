0b3736c6b0fa3f8db8136d53ef7bbabfea1b54d7
Vinay Vinnu <vinaymugada007@gmail.com>
2023-11-09 18:14:27 +0530

    Dockerfile is commited

---
+++ docker_assignment/docker_assignment/src/main/java/com/docker/assignment/docker_assignment/DockerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.docker.assignment.docker_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAssignmentApplication.class, args);
+	}
+
+}
---
+++ docker_assignment/docker_assignment/src/test/java/com/docker/assignment/docker_assignment/DockerAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.assignment.docker_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
